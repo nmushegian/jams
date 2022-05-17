c48d671b4ad6b4df4318337e8f34d3a58e564420
dmfxyz <100147743+dmfxyz@users.noreply.github.com>
2022-05-16 22:52:35 -0400

    Fix file tests (#17)
    * str and q_str checking for appropriate children
    
    * fix file testing

--- test/test.js
+++ test/test.js
@@ -5,14 +5,17 @@ import { jams, read } from '../jams.js'
 import { readFileSync, readdirSync } from 'fs'
 
 test('passing files and their JSON equivalents', t=> {
-    readdirSync('./test/pass', filename=>{
-        const jams_o = jams(readFileSync('./pass/${filename}'))
-        const jsonfile = readFileSync('./pass/${filename}.json')
-        if (jsonfile) {
-            const json_o = JSON.parse(jsonfile)
-            for (const key in json_o) {
-                t.ok(jams_o[key])
-                t.ok(obj_equals(json_o[key], jams_o[key]))
+    readdirSync('./test/pass').forEach(filename => {
+        const extension = filename.split('.').slice(-1)
+        if (extension == "jams"){ // only read JAMS, look for json if jams exists
+            const jams_o = jams(readFileSync(`./test/pass/${filename}`))
+            const jsonfile = readFileSync(`./test/pass/${filename}`.replace('jams', 'json'))
+            if (jsonfile) {
+                const json_o = JSON.parse(jsonfile)
+                for (const key in json_o) {
+                    t.ok(jams_o[key])
+                    t.ok(obj_equals(json_o[key], jams_o[key]))
+                }
             }
         }
     })
